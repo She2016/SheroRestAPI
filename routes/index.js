8ca8e7d48289a42ab950ac132d164caa4e41cb82
Sheyar <ali.sheyar@gmail.com>
2018-09-20 16:45:55 +0200

    first commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/shero', function(req, res, next) {
+  res.render('shero', { title: 'Sheyarrrrrrrrrrrrr' });
+});
+
 module.exports = router;
